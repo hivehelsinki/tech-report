76b659271aa82222e9fc5e59f14b4484890a2081
Titus <jgengo@student.42.fr>
2023-04-20 18:56:45 +0300

    ui: landing page turned into components showcases for dev

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,26 @@ export default function Home() {
   return (
     <main className="p-2">
       <h1>Next.js + Tailwind CSS</h1>
-      <Button>component example</Button>
-      <div className="flex gap-3 flex-row">
-        <Badge />
-        <Badge variant="ongoing" />
-        <Badge variant="resolved" />
+
+      <h2 className="text-2xl font-bold my-5">Components</h2>
+
+      <div id="wrapper" className="flex flex-wrap">
+        <div className="basis-1/3">
+          <h3 className="font-bold text-xl mb-3">Buttons</h3>
+          <Button>component example</Button>
+        </div>
+
+        <div className="basis-1/3">
+          <h3 className="font-bold text-xl mb-3">Badges</h3>
+          <div className="flex gap-3 flex-row">
+            <Badge />
+            <Badge variant="ongoing" />
+            <Badge variant="resolved" />
+          </div>
+        </div>
+        <div className="basis-1/3">
+          <h3 className="font-bold text-xl mb-3">..</h3>
+        </div>
       </div>
     </main>
   );

ce3c1cd11fa9ddb4782636f8dfbc7614f4ac313f
Jordane Gengo <jordane@hive.fi>
2023-04-20 15:37:49 +0300

    feat: edit default tailwind container class

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {},
   },
   plugins: [],

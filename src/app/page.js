363cae5628473d3448892e23b3cb6847abc839cd
Amedeo Majer <amedeomajer@Amedeos-MacBook-Pro.local>
2023-04-20 15:06:14 +0300

    feat(Badge) : created Badge component file and added it to app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Button from '../components/Button';
+import Badge from '../components/Badge';
+import Accordion from '../components/Accordion';
 
 export default function Home() {
   return (
     <main className="p-2">
       <h1>Next.js + Tailwind CSS</h1>
       <Button>component example</Button>
+      <Accordion></Accordion>
+      <Badge>badge example</Badge>
     </main>
   );
 }
---
+++ src/components/Badge.jsx
@@ -0,0 +1,9 @@
+import { cva } from 'class-variance-authority';
+
+// const badgeVariants = cva{
+// 	"ra"
+// };
+
+export default function Badge({ children }) {
+  return <div>{children}</div>;
+}

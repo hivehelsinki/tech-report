a38641dc7b5b13a608de3a3e8fa64e4af22a134c
Amedeo Majer <amedeomajer@Amedeos-MacBook-Pro.local>
2023-04-20 16:33:22 +0300

    fix: removed Accordion import on page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,11 @@
 import Button from '../components/Button';
 import Badge from '../components/Badge';
-import Accordion from '../components/Accordion';
 
 export default function Home() {
   return (
     <main className="p-2">
       <h1>Next.js + Tailwind CSS</h1>
       <Button>component example</Button>
-      <Accordion></Accordion>
       <div className="flex gap-3 flex-row">
         <Badge />
         <Badge variant="ongoing" />

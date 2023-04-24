331fee483defd369314d2ccd6767fe9b74c9d8c6
Amedeo Majer <amedeo@hive.fi>
2023-04-24 10:14:43 +0300

    feat: installed radio radix ui component, created select device molecule

--- src/components/ReportForm.jsx
+++ src/components/ReportForm.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+'use client';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import SelectDevice from './SelectDevice';
 
 const ReportForm = () => {
   return (
-    <div>ReportForm</div>
-  )
-}
+    <form className="">
+      <SelectDevice />
+    </form>
+  );
+};
 
-export default ReportForm
+export default ReportForm;
---
+++ src/components/SelectDevice.jsx
@@ -0,0 +1,35 @@
+'use client';
+import * as RadioGroup from '@radix-ui/react-radio-group';
+import { CheckIcon } from '@radix-ui/react-icons';
+
+const SelectDevice = () => {
+  const devices = ['iMac', 'Mouse', 'Keyboard'].map((device) => {
+    return (
+      <RadioGroup.Item
+        className="inline-flex items-center"
+        value={device}
+        id={device}
+        key={device}
+      >
+        <div className="flex h-9 w-9 items-center justify-center border">
+          <RadioGroup.Indicator>
+            <CheckIcon className="h-9 w-9 text-slate-400" />
+          </RadioGroup.Indicator>
+        </div>
+        <label className="pl-4" htmlFor="r1">
+          {device}
+        </label>
+      </RadioGroup.Item>
+    );
+  });
+  return (
+    <>
+      <label className="font-semibold">Select a device</label>
+      <RadioGroup.Root className="mt-4 flex flex-col space-y-4">
+        {devices}
+      </RadioGroup.Root>
+    </>
+  );
+};
+
+export default SelectDevice;

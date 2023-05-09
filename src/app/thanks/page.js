a3013b191a8fa13ebab1420dea964691d8ea5daf
Amedeo Majer <amedeo@hive.fi>
2023-05-09 12:52:17 +0300

    fix: protected /thanks page from non logged in users

--- src/app/thanks/page.js
+++ src/app/thanks/page.js
@@ -1,9 +1,14 @@
 import Button from '@components/Button';
 import Image from 'next/image';
 import success from '@public/success.svg';
+import { getCurrentUser } from '@/lib/session';
+import { redirect } from 'next/navigation';
 
 // TODO: transition from top for the checkmark
-const Success = () => {
+const Success = async () => {
+  const user = await getCurrentUser();
+
+  if (!user) redirect('/login');
   return (
     <div className="container flex h-screen flex-col items-center justify-center gap-10">
       <Image

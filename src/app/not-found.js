4f384102c72b15ca60618137538f753aca7c8c26
Amedeo Majer <amedeo@hive.fi>
2023-05-09 14:48:09 +0300

    feat: reduced top margin in page so thay it is more at eye level in desktop and easier to press home in mobile

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 export default async function NotFound() {
   return (
-    <main className="p-2">
-      <div className="flex h-screen flex-col items-center justify-center gap-10 text-hdark lg:px-24">
+    <main className="p-2 pt-20">
+      <div className="flex h-full flex-col items-center justify-center gap-12 text-hdark lg:px-24">
         <div className="relative">
           <Image src={notFound} alt="404" width={900} height={743} />
           <p className="absolute right-4 top-0 text-7xl font-semibold mix-blend-overlay md:text-9xl">

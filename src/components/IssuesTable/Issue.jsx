f841f717b4be03f95f32bdfc30ab78b04a78f5f6
Amedeo Majer <amedeo@hive.fi>
2023-05-09 12:59:06 +0300

    fix: changed hostname min-width so that all devices are in line

--- src/components/IssuesTable/Issue.jsx
+++ src/components/IssuesTable/Issue.jsx
@@ -35,7 +35,7 @@ const Issue = ({ props }) => {
       <Accordion.Header className="group relative flex w-full items-center justify-between border-b px-5 py-4">
         <Accordion.Trigger className="group flex w-full items-center justify-between">
           <div className="inline-flex w-2/5 items-center gap-8">
-            <p>{issue.host}</p> <p>{issue.device}</p>
+            <p className="min-w-[65px]">{issue.host}</p> <p>{issue.device}</p>
           </div>
           <ChevronDownIcon className="h-5 w-5 transition-transform duration-300 group-data-[state=open]:rotate-180" />
         </Accordion.Trigger>

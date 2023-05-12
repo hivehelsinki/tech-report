168871a83b5c35dc200024e468ff22bd9b0d9ab2
Jordane Gengo <jordane@hive.fi>
2023-05-12 13:23:51 +0300

    feat: delete issues feature

--- src/components/IssuesTable/SelectStatus.jsx
+++ src/components/IssuesTable/SelectStatus.jsx
@@ -18,31 +18,37 @@ const SelectStatus = ({ user, status, handleStatus, open, setOpen }) => {
         </PopoverTrigger>
         {user.admin && (
           <PopoverContent
-            className=" flex w-[110px] flex-col gap-2 text-sm"
+            className=" flex w-[110px] flex-col gap-2 divide-y text-sm"
             side="bottom"
             align="start"
             onClick={(event) =>
               handleStatus(event.target.getAttribute('value'))
             }
           >
-            <p
-              value="open"
-              className="cursor-pointer hover:font-semibold hover:text-slate-500"
-            >
-              Open
-            </p>
-            <p
-              value="ongoing"
-              className="cursor-pointer hover:font-semibold hover:text-horange"
-            >
-              Ongoing
-            </p>
-            <p
-              value="resolved"
-              className="cursor-pointer hover:font-semibold hover:text-hgreen"
-            >
-              Resolved
-            </p>
+            <div className="flex flex-col gap-2">
+              <p
+                value="open"
+                className="cursor-pointer hover:font-semibold hover:text-slate-500"
+              >
+                Open
+              </p>
+              <p
+                value="ongoing"
+                className="cursor-pointer hover:font-semibold hover:text-horange"
+              >
+                Ongoing
+              </p>
+              <p
+                value="resolved"
+                className="cursor-pointer hover:font-semibold hover:text-hgreen"
+              >
+                Resolved
+              </p>
+            </div>
+
+            <div className="cursor-pointer pt-2 text-red-600 hover:font-semibold">
+              <p value="delete">Delete</p>
+            </div>
           </PopoverContent>
         )}
       </Popover>

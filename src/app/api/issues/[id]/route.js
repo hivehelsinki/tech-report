31ed4ba362352b497d5594b85368a2c8eeb1710d
Jordane Gengo <jordane@hive.fi>
2023-05-12 13:25:47 +0300

    Revert "feat: delete issues feature"
    This reverts commit 168871a83b5c35dc200024e468ff22bd9b0d9ab2.

--- src/app/api/issues/[id]/route.js
+++ src/app/api/issues/[id]/route.js
@@ -14,7 +14,7 @@ export async function GET(request, { params }) {
     });
   } catch (error) {
     console.log(`#########\n ${error.message} \n#########`);
-    return new Response('', { status: 500 });
+    return new Response({}, { status: 500 });
   }
 }
 
@@ -32,30 +32,13 @@ export async function PATCH(request, { params }) {
           closed: issue.status === 'resolved' ? new Date() : null,
         },
       });
-      return new Response('', { status: 200 });
+      return new Response({}, { status: 200 });
     } catch (error) {
       console.log(`#########\n ${error.message} \n#########`);
-      return new Response('', { status: 500 });
+      return new Response({}, { status: 500 });
     }
   } else {
     console.log(`#########\n UNAUTORISED \n#########`);
-    return new Response('', { status: 401 });
-  }
-}
-
-export async function DELETE(request, { params }) {
-  const user = await getCurrentUser(request);
-  if (user && user.admin) {
-    try {
-      await prisma.Issue.delete({
-        where: {
-          id: Number(params.id),
-        },
-      });
-      return new Response('', { status: 200 });
-    } catch (error) {
-      console.log(`#########\n ${error.message} \n#########`);
-      return new Response('', { status: 500 });
-    }
+    return new Response({}, { status: 401 });
   }
 }

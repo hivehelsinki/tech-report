7a8f87c7cf58dc7c1b73da78b257c674c1693646
Titus <jgengo@student.42.fr>
2023-05-21 19:49:24 +0300

    feat: migrate button to tailwind variants

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,30 +1,24 @@
-import { cn } from '../lib/utils';
-import { cva } from 'class-variance-authority';
+import { tv } from 'tailwind-variants';
 
-const buttonVariants = cva(
-  'inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
-  {
-    variants: {
-      variant: {
-        default: 'bg-hyellow hover:bg-hyellow/90',
-      },
-      size: {
-        default: 'h-11 px-8',
-      },
+const button = tv({
+  base: 'inline-flex items-center justify-center text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  variants: {
+    variant: {
+      default: 'bg-hyellow hover:bg-hyellow/90',
     },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
+    size: {
+      default: 'h-11 px-8',
     },
-  }
-);
+  },
+  defaultVariants: {
+    variant: 'default',
+    size: 'default',
+  },
+});
 
 const Button = ({ children, className, size, variant, ...props }) => {
   return (
-    <button
-      className={cn(buttonVariants({ size, variant, className }))}
-      {...props}
-    >
+    <button className={button({ className, size, variant })} {...props}>
       {children}
     </button>
   );

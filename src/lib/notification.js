9317d4f2c570e7fbe819c6c979be3a12b0c092e7
Jordane Gengo <jordane@hive.fi>
2023-06-06 14:41:47 +0300

    chore: add docs to functions

--- src/lib/notification.js
+++ src/lib/notification.js
@@ -1,6 +1,12 @@
 import { slack_notification } from './slack';
 import { discord_notification } from './discord';
 
+/**
+ * Function to send a notification to Slack and Discord
+ * @param type The type of notification to send
+ * @param data The data to send in the notification
+ * @returns null
+ */
 export function notification(type, data) {
   slack_notification(type, data);
   discord_notification(type, data);

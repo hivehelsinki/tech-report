c9ab1167f406679cd53cfb317509c7a93f0cd187
Jordane Gengo <jordane@hive.fi>
2023-06-15 09:25:06 +0300

    fix: hydration issue because the theme is not known until the client mounted the component

--- src/components/ToggleTheme.jsx
+++ src/components/ToggleTheme.jsx
@@ -1,13 +1,20 @@
 'use client';
+
+import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import { Sun, Moon } from 'lucide-react';
 
 const ToggleTheme = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
 
   const handleClick = () => {
     theme === 'dark' ? setTheme('light') : setTheme('dark');
   };
+
+  if (!mounted) return null;
 
   return (
     <button onClick={handleClick}>
